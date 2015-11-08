f761f8864faab1cc05477e618e4bf60cca637b3b
Ben Lipton <ben.lipton@gmail.com>
2015-11-08 17:14:52 -0500

    Modexp is now working
    Had to abstract away several bignum methods

--- encrypt.c
+++ encrypt.c
@@ -13,17 +13,67 @@ typedef int16_t halfwordsigned;  // a halfword plus sign bit
 typedef struct bignum {
 				halfword *num;
 				unsigned int length;
+        unsigned int offset;
 } bignum;
+
+
+void bignum_set(bignum *bn, int index, halfword val) {
+  assert(bn->offset + index < bn->length);
+  bn->num[index+bn->offset] = val;
+}
+
+halfword bignum_index(const bignum *bn, int index) {
+  assert(bn->offset + index < bn->length);
+  return bn->num[bn->offset + index];
+}
+
+int bignum_size(const bignum *bn) {
+  return bn->length - bn->offset;
+}
+
+int bignum_setsize(bignum *bn, int size) {
+  assert(size <= bn->length);
+  bn->offset = bn->length - size;
+}
+
+void bignum_zero(bignum *bn) {
+  memset(bn->num, 0, bn->length);
+}
+
+void find_topbit(const bignum *bn, int *topbyte, int *topbit) {
+  halfword temp;
+
+  for (*topbyte = 0; *topbyte < bignum_size(bn); (*topbyte)++) {
+    if (bignum_index(bn, *topbyte) != 0) {
+      break;
+    }
+  }
+
+  temp = bignum_index(bn, *topbyte);
+  for (*topbit = 7; *topbit >= 0; (*topbit)--) {
+    if (temp & (1<<*topbit)) {
+      break;
+    }
+  }
+}
+
+void bignum_truncate(bignum *bn) {
+  int topbit;
+  int topbyte;
+  bn->offset = 0;
+  // TODO: computing and passing topbit is unnecessary work
+  find_topbit(bn, &topbyte, &topbit);
+  bn->offset = topbyte;
+}
 
 void bignum_print(bignum *bn, char *label) {
   int i;
   printf("%s0x", label);
-  for (i = 0; i < bn->length; i++) {
-					printf("%02x", bn->num[i]);
+  for (i = 0; i < bignum_size(bn); i++) {
+					printf("%02x", bignum_index(bn, i));
 	}
 	printf("\n");
 }
-
 
 /* multiply: multiplies in1 and in2, returns result in out.
  *  in1: bignum of length k1
@@ -35,29 +85,33 @@ void bignum_multiply(bignum *out, const bignum *in1, const bignum *in2) {
   int j;
   halfword C;
   word intermediate;
-  int offset = out->length - in1->length - in2->length + 1;
+  //int offset = bignum_size(out) - bignum_size(in1) - bignum_size(in2) + 1;
 
-  assert(out->length >= in1->length + in2->length);
   assert(out != in1);
   assert(out != in2);
 
-  memset(out->num, 0, out->length);
-  for (i = in2->length-1; i >= 0; i--) {
+  bignum_setsize(out, bignum_size(in1) + bignum_size(in2));
+  bignum_zero(out);
+  for (i = bignum_size(in2)-1; i >= 0; i--) {
     C = 0;
-    for (j = in1->length-1; j >= 0; j--) {
-      intermediate = out->num[i+j+offset] +
-        in1->num[j]*in2->num[i] + C;
-      C = intermediate >> 8*sizeof(*out->num);
-      out->num[i+j+offset] = intermediate;
+    for (j = bignum_size(in1)-1; j >= 0; j--) {
+      intermediate = bignum_index(out, i+j+1) +
+        bignum_index(in1, j)*bignum_index(in2, i) + C;
+      C = intermediate >> 8*sizeof(halfword);
+      bignum_set(out, i+j+1, intermediate);
     }
-    out->num[i+offset-1] = C;
+    bignum_set(out, i, C);
   }
 }
 
 void bignum_copy(bignum *dst, bignum *src) {
   // TODO: this might be wrong if length is measured in halfwords, not bytes
-  memcpy(&dst->num[dst->length - src->length], src->num, src->length);
-  memset(dst->num, 0, dst->length - src->length);
+  int src_size = bignum_size(src);
+  assert(dst->length >= src_size);
+
+  memcpy(&dst->num[dst->length - src_size], &src->num[src->offset], src_size);
+  memset(dst->num, 0, dst->length - src_size);
+  bignum_truncate(dst);
 }
 
 int maybe_subtract(bignum *out, bignum *n, bignum *temp,
@@ -68,30 +122,28 @@ int maybe_subtract(bignum *out, bignum *n, bignum *temp,
   halfword effective_nbyte;
   halfwordsigned result = 0;
 
-  assert(out->length >= n->length + byteshift);
-  assert(temp->length == out->length);
+  assert(bignum_size(out) >= bignum_size(n) + byteshift);
 
   bignum_copy(temp, out);
 
-  for (out_index = out->length-1-byteshift; out_index >= 0; out_index--) {
-    n_index = out_index - (out->length - n->length) + byteshift;
+  for (out_index = bignum_size(out)-1-byteshift; out_index >= 0; out_index--) {
+    n_index = out_index - (bignum_size(out) - bignum_size(n)) + byteshift;
+    effective_nbyte = 0;
     if (n_index >= 0) {
-      effective_nbyte = n->num[n_index] << bitshift;
-      if (n_index < n->length-1) {
-        effective_nbyte |= n->num[n_index+1] >> (8-bitshift);
-      }
-    } else {
-      effective_nbyte = 0;
+      effective_nbyte |= bignum_index(n, n_index) << bitshift;
+    }
+    if (n_index < bignum_size(n)-1) {
+      effective_nbyte |= bignum_index(n, n_index+1) >> (8-bitshift);
     }
 
-    result = out->num[out_index] - effective_nbyte - borrow;
+    result = bignum_index(out, out_index) - effective_nbyte - borrow;
     if (result < 0) {
       result += 1<<(8*sizeof(halfword));
       borrow = 1;
     } else {
       borrow = 0;
     }
-    out->num[out_index] = result;
+    bignum_set(out, out_index, result);
   }
 
   // If the result is negative, we need to undo the subtraction
@@ -103,22 +155,6 @@ int maybe_subtract(bignum *out, bignum *n, bignum *temp,
   return !borrow;
 }
 
-void find_topbit(bignum *bn, int *topbyte, int *topbit) {
-  halfword temp;
-
-  for (*topbyte = 0; *topbyte < bn->length; (*topbyte)++) {
-    if (bn->num[*topbyte] != 0) {
-      break;
-    }
-  }
-
-  temp = bn->num[*topbyte];
-  for (*topbit = 7; *topbit >= 0; (*topbit)--) {
-    if (temp & (1<<*topbit)) {
-      break;
-    }
-  }
-}
 
 /* mod: divides t by n and returns result in out.
  *  t: bignum of maximum length 2k
@@ -138,15 +174,13 @@ void bignum_mod(bignum *out, bignum *t, bignum *n, bignum *temp) {
   int n_topbyte;
   int n_topbit;
 
-  assert(t->length <= n->length * 2);
-  assert(out->length >= n->length *2);
-  assert(temp->length == out->length);
+  assert(temp->length >= bignum_size(t));
 
   bignum_copy(out, t);
 
   find_topbit(out, &out_topbyte, &out_topbit);
   find_topbit(n, &n_topbyte, &n_topbit);
-  byteshift = out->length - n->length - out_topbyte + n_topbyte;
+  byteshift = bignum_size(out) - bignum_size(n) - out_topbyte + n_topbyte;
   bitshift = out_topbit - n_topbit;
   if (bitshift < 0) {
     bitshift += 8;
@@ -161,20 +195,23 @@ void bignum_mod(bignum *out, bignum *t, bignum *n, bignum *temp) {
       byteshift--;
     }
   }
+
+  bignum_truncate(out);
 }
 
 void bignum_modexp(bignum *out, bignum *M, bignum *e, bignum *n, bignum *temp1, bignum *temp2) {
-  memset(out->num, 0, out->length);
-  out->num[out->length - 1] = 1;
-
   int e_byte;
   int e_bit;
+
+  bignum_setsize(out, 1);
+  bignum_set(out, 0, 1);
+
   find_topbit(e, &e_byte, &e_bit);
 
   while (e_byte >= 0) {
     bignum_multiply(temp1, out, out);
     bignum_mod(out, temp1, n, temp2);
-    if (e->num[e_byte] & 1<<e_bit) {
+    if (bignum_index(e, e_byte) & 1<<e_bit) {
       bignum_multiply(temp1, out, M);
       bignum_mod(out, temp1, n, temp2);
     }
@@ -186,30 +223,23 @@ void bignum_modexp(bignum *out, bignum *M, bignum *e, bignum *n, bignum *temp1, 
   }
 }
 
-/*
-void rsa_perform(const unsigned char *e, const int e_length,
-								const unsigned char *n, const int n_length,
-								const unsigned char *in, const int in_length,
-								unsigned char *out, const int out_length) {
-				unsigned char *result = malloc(n_length);
-				unsigned char *temp = malloc(n_length);
-				multiply(result, n_length, in, in_length, in, in_length);
-}
-*/
-
 int main(int argc, char *argv[]) {
 	unsigned char a_num[] = {0x12, 0x34};
-	unsigned char e_num[] = {0x03};
+	unsigned char e_num[] = {0x04};
   unsigned char n_num[] = {0x05, 0xea};
 	unsigned char out_num[2*sizeof(n_num)];
   unsigned char temp1_num[sizeof(out_num)];
   unsigned char temp2_num[sizeof(out_num)];
-  bignum a = {a_num, sizeof(a_num)};
-  bignum e = {e_num, sizeof(e_num)};
-  bignum n = {n_num, sizeof(n_num)};
-  bignum out = {out_num, sizeof(out_num)};
-  bignum temp1 = {temp1_num, sizeof(temp1_num)};
-  bignum temp2 = {temp2_num, sizeof(temp2_num)};
+  bignum a = {a_num, sizeof(a_num), 0};
+  bignum e = {e_num, sizeof(e_num), 0};
+  bignum n = {n_num, sizeof(n_num), 0};
+  bignum out = {out_num, sizeof(out_num), 0};
+  bignum temp1 = {temp1_num, sizeof(temp1_num), 0};
+  bignum temp2 = {temp2_num, sizeof(temp2_num), 0};
+
+  bignum_truncate(&out);
+  bignum_truncate(&temp1);
+  bignum_truncate(&temp2);
 
   bignum_modexp(&out, &a, &e, &n, &temp1, &temp2);
 	bignum_print(&out, "");
