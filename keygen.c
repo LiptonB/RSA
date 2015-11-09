a508514aea57c32da5815af69d0d93f92370c264
Ben Lipton <ben.lipton@gmail.com>
2015-11-09 09:16:32 -0500

    Add PEM output to keygen and re-run

--- keygen.c
+++ keygen.c
@@ -1,7 +1,8 @@
 #include <openssl/rsa.h>
 #include <openssl/engine.h>
+#include <openssl/pem.h>
 
-void print_array(char *name, BIGNUM *bn) {
+void fprint_array(FILE *fp, char *name, BIGNUM *bn) {
   unsigned char *buf;
   int len;
   int i;
@@ -9,28 +10,37 @@ void print_array(char *name, BIGNUM *bn) {
   if ((buf = malloc(BN_num_bytes(bn))) != NULL) {
     len = BN_bn2bin(bn, buf);
 
-    printf("unsigned char %s[] = {", name);
+    fprintf(fp, "unsigned char key_%s[] = {", name);
     for (i = 0; i < len; i++) {
-      printf("0x%02x", buf[i]);
+      fprintf(fp, "0x%02x", buf[i]);
       if (i < len-1) {
-        printf(", ");
+        fprintf(fp, ", ");
       }
     }
-    printf("};\n");
+    fprintf(fp, "};\n");
 
     free(buf);
   }
 }
 
 int main(int argc, char *argv[]) {
+  FILE *fp = fopen("key.c", "w");
   RSA *keypair = RSA_generate_key(1024, 65537, NULL, NULL);
-  print_array("n", keypair->n);
-  print_array("e", keypair->e);
-  print_array("d", keypair->d);
-  print_array("p", keypair->d);
-  print_array("q", keypair->d);
-  print_array("dmp1", keypair->d);
-  print_array("dmq1", keypair->d);
-  print_array("iqmp", keypair->d);
+  fprint_array(fp, "n", keypair->n);
+  fprint_array(fp, "e", keypair->e);
+  fprint_array(fp, "d", keypair->d);
+  fprint_array(fp, "p", keypair->d);
+  fprint_array(fp, "q", keypair->d);
+  fprint_array(fp, "dmp1", keypair->d);
+  fprint_array(fp, "dmq1", keypair->d);
+  fprint_array(fp, "iqmp", keypair->d);
+  fclose(fp);
+
+  fp = fopen("key.pub.pem", "w");
+  PEM_write_RSAPrivateKey(fp, keypair, NULL, NULL, 0, NULL, NULL);
+  fclose(fp);
+  fp = fopen("key.priv.pem", "w");
+  PEM_write_RSAPublicKey(fp, keypair);
+  fclose(fp);
   return 0;
 }
