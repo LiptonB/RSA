LiptonB/RSA

commit 6057b67bf046eb674fe60d865db8199c33d946bf

Make input in main more flexible

cli_main.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "encrypt.h"
 #include "key.h"
 
 int main(int argc, char *argv[]) {
-	unsigned char input_num[128];
+  unsigned char input_num[128];
   char input_str[258];
-	unsigned char out_num[2*key_n_size];
-  unsigned char temp1_num[sizeof(out_num)];
-  unsigned char temp2_num[sizeof(out_num)];
+  size_t buffer_size = 2 * key_n_size;
+  unsigned char *out_num = malloc(buffer_size);
+  unsigned char *temp1_num = malloc(buffer_size);
+  unsigned char *temp2_num = malloc(buffer_size);
   bignum in = {input_num, sizeof(input_num), 0};
   bignum e = {key_e, key_e_size, 0};
   bignum d = {key_d, key_d_size, 0};
   bignum n = {key_n, key_n_size, 0};
-  bignum out = {out_num, sizeof(out_num), 0};
+  bignum out = {out_num, buffer_size, 0};
   bignum temp1 = {temp1_num, sizeof(temp1_num), 0};
   bignum temp2 = {temp2_num, sizeof(temp2_num), 0};
   int str_idx, num_idx;
@@ -30,5 +32,5 @@ int main(int argc, char *argv[]) {
   bignum_print(&in, "input: ");
 
   bignum_modexp(&out, &in, &e, &n, &temp1, &temp2);
-	bignum_print(&out, "");
+  bignum_print(&out, "");
 }

RSA-MSP430/main.c
@@ -3,8 +3,10 @@
 #include "encrypt.h"
 #include "key.h"
 
-unsigned char input_num[] = {0x12, 0x34};
-bignum in = {input_num, sizeof(input_num), 0};
+const unsigned char input_val[] = {0x12, 0x34};
+
+unsigned char input_num[128];
+bignum in = {input_num, sizeof(input_num), sizeof(input_num)-sizeof(input_val)};
 volatile int timer;
 
 #pragma vector=TIMERA0_VECTOR
@@ -15,6 +17,8 @@ __interrupt void inc_timer(void) {
 #define BIGNUM_SIZE 256
 
 int main(void) {
+	unsigned int i;
+
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 
     // Make system clock as fast as it will go
@@ -36,6 +40,10 @@ int main(void) {
     bignum out = {out_num, BIGNUM_SIZE, 0};
     bignum temp1 = {temp1_num, BIGNUM_SIZE, 0};
     bignum temp2 = {temp2_num, BIGNUM_SIZE, 0};
+
+    for (i = 0; i < sizeof(input_val); i++) {
+    	bignum_set(&in, i, input_val[i]);
+    }
 
     // Set up and start 256KHz timer
     TACTL = TASSEL_2 | ID_3;

encrypt.h
@@ -16,6 +16,7 @@ typedef struct bignum {
 
 void bignum_modexp(bignum *out, const bignum *M, const bignum *e, const bignum *n, bignum *temp1, bignum *temp2);
 void bignum_truncate(bignum *bn);
+void bignum_set(bignum *bn, int index, halfword val);
 
 #ifdef WITH_PRINTF
 void bignum_print(bignum *bn, char *label);

commit 0b066227a66f18653524f6229c93ca95f82aa27d

Fix incorrect size introduced in 6057b67

cli_main.c
@@ -16,8 +16,8 @@ int main(int argc, char *argv[]) {
   bignum d = {key_d, key_d_size, 0};
   bignum n = {key_n, key_n_size, 0};
   bignum out = {out_num, buffer_size, 0};
-  bignum temp1 = {temp1_num, sizeof(temp1_num), 0};
-  bignum temp2 = {temp2_num, sizeof(temp2_num), 0};
+  bignum temp1 = {temp1_num, buffer_size, 0};
+  bignum temp2 = {temp2_num, buffer_size, 0};
   int str_idx, num_idx;
 
   fgets(input_str, sizeof(input_str), stdin);
@@ -33,4 +33,8 @@ int main(int argc, char *argv[]) {
 
   bignum_modexp(&out, &in, &e, &n, &temp1, &temp2);
   bignum_print(&out, "");
+
+  free(out_num);
+  free(temp1_num);
+  free(temp2_num);
 }