38a5ee208f6d0f25b39ac77c0886e7883fcd0d16
Ben Lipton <ben.lipton@gmail.com>
2015-12-01 21:29:18 -0500

    Make values in key.h const

--- key.h
+++ key.h
@@ -3,21 +3,21 @@
 
 #include <stddef.h>
 
-extern unsigned char key_n[];
-extern size_t key_n_size;
-extern unsigned char key_e[];
-extern size_t key_e_size;
-extern unsigned char key_d[];
-extern size_t key_d_size;
-extern unsigned char key_p[];
-extern size_t key_p_size;
-extern unsigned char key_q[];
-extern size_t key_q_size;
-extern unsigned char key_dmp1[];
-extern size_t key_dmp1_size;
-extern unsigned char key_dmq1[];
-extern size_t key_dmq1_size;
-extern unsigned char key_iqmp[];
-extern size_t key_iqmp_size;
+extern const unsigned char key_n[];
+extern const size_t key_n_size;
+extern const unsigned char key_e[];
+extern const size_t key_e_size;
+extern const unsigned char key_d[];
+extern const size_t key_d_size;
+extern const unsigned char key_p[];
+extern const size_t key_p_size;
+extern const unsigned char key_q[];
+extern const size_t key_q_size;
+extern const unsigned char key_dmp1[];
+extern const size_t key_dmp1_size;
+extern const unsigned char key_dmq1[];
+extern const size_t key_dmq1_size;
+extern const unsigned char key_iqmp[];
+extern const size_t key_iqmp_size;
 
 #endif
